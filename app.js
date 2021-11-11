5306377c8b529b7bdbbd61e39140707a3e189a94
Khushnood Asif <khushnood99@gmail.com>
2021-11-11 17:45:01 +0000

    fix port issue

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.post("/failure", function(req, res) {
    res.redirect("/");
 });
 
-app.listen(process.env.POST || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
    console.log("Server started on port 3000");
 });
