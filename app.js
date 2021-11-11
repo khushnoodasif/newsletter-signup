4123bb9e7a687f5f7e62a8a3ca3a3ebfea77d486
Khushnood Asif <khushnood99@gmail.com>
2021-11-11 18:02:06 +0000

    updated gitignore

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.post("/failure", function(req, res) {
    res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT, function () {
    console.log("Server started on port 3000");
 });
