5d2b397c5fe441396d1f982061c98818a4cdf301
Khushnood Asif <khushnood99@gmail.com>
2021-11-11 00:39:16 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+   const firstName = req.body.fName;
+   const lastName = req.body.lName;
+   const email = req.body.email;
+
+   const data = {
+      members: [{email_address: email, status: "subscribed", merge_fields: {FNAME: firstName, LNAME: lastName}}]
+   }
+
+   const jsonData = JSON.stringify(data);
+   const url = "https://us20.api.mailchimp.com/3.0/lists/e6d92b7f7a"
+   const options = {
+      method: "POST",
+      auth: "khushnooasif:68e173f49af0d232b1fc00ce851a989b-us20"
+   }
+
+   const request = https.request(url, options, function(response) {
+      response.on("data", function(data){
+         console.log(JSON.parse(data));
+      })
+   })
+
+   request.write(jsonData);
+   request.end();
+
+});
+
+app.listen(3000, function () {
+   console.log("Server started on port 3000");
+});
